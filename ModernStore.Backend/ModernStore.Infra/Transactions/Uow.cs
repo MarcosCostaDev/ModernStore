e7caa129f151e2d245401970ebdbbf5799cc1835
Marcos Costa <marcos.lcosta@live.com>
2017-08-29 19:06:49 -0300

    Update Uow.cs

--- ModernStore.Backend/ModernStore.Infra/Transactions/Uow.cs
+++ ModernStore.Backend/ModernStore.Infra/Transactions/Uow.cs
@@ -16,36 +16,8 @@ namespace ModernStore.Infra.Transactions
         }
 
         public void Commit()
-        {
-            try
-            {
-                _context.SaveChanges();
-            }
-            catch (DbEntityValidationException e)
-            {
-                foreach (var eve in e.EntityValidationErrors)
-                {
-                    Debug.WriteLine(@"Entity of type ""{0}"" in state ""{1}"" 
-                   has the following validation errors:",
-                        eve.Entry.Entity.GetType().Name,
-                        eve.Entry.State);
-                    foreach (var ve in eve.ValidationErrors)
-                    {
-                        Debug.WriteLine(@"- Property: ""{0}"", Error: ""{1}""",
-                            ve.PropertyName, ve.ErrorMessage);
-                    }
-                }
-                throw;
-            }
-            catch (DbUpdateException e)
-            {
-                Debug.WriteLine(e.Message);
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine(e.Message);
-                throw;
-            }
+        {           
+           _context.SaveChanges();
         }
 
         public void Rollback()
